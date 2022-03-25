e27b8b92b0f5e3f5dc7c3d2229a5c27565f7a2f4
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-03-25 17:38:54 -0300

    Delete TadMatriz directory

--- TadMatriz/matriz.h
+++
@@ -1,9 +0,0 @@
-typedef struct matriz Matriz;
-
-Matriz* criar(int, int);
-void destruir(Matriz*);
-float acessarValor(Matriz*, int, int);
-int numeroLinhas(Matriz*);
-int numeroColunas(Matriz*);
-void atribuir(Matriz*, int, int, float);
-void imprimir(Matriz*);
