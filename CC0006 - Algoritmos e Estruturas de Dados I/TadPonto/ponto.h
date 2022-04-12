175f3b4c279a1cf2c1f964488b8c8b8a1fb130f3
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-04-12 11:28:53 -0300

    Delete CC0006 - Algoritmos e Estruturas de Dados I directory

--- CC0006 - Algoritmos e Estruturas de Dados I/TadPonto/ponto.h
+++
@@ -1,8 +0,0 @@
-typedef struct ponto Ponto;
-
-Ponto* criar(float, float);
-void destruir(Ponto*);
-void acessar(Ponto*, float*, float*);
-void atribuir(Ponto*, float, float);
-void exibir(Ponto*);
-float distancia(Ponto*, Ponto*);
